d725cf7e7ec0fa48100165050b08c5c01e2c4fb5
Abhijeet Gupta <44775514+abhijeet010304@users.noreply.github.com>
2021-03-31 22:09:35 +0530

    Update cycleDetectionUsingDFS.cpp

--- graph/cycleDetectionUsingDFS.cpp
+++ graph/cycleDetectionUsingDFS.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 #define pb push_back
 vector<int>g[100005];
-bool vis[100005];
+
 void addEdge(int u,int v){
     g[u].pb(v);
-    g[v].pb(u);
+    // g[v].pb(u);
 }
 
 
@@ -21,16 +21,16 @@ void clearall(int n){
     for(int i=0;i<n;i++){
         g[i].clear();
     }
-    memset(vis,false,sizeof(vis));
+    // memset(vis,false,sizeof(vis));
 
 }
 
-bool dfs(int c,int p){
-    vis[c]=true;
+bool dfs(int c,int p,vector<bool> vis){
+    vis[c] = true;
     for(auto x :g[c]){
         if(!vis[x]){
             bool res;
-            res = dfs(x,c);
+            res = dfs(x,c,vis);
             if(res==true)return true;
 
 
@@ -39,18 +39,24 @@ bool dfs(int c,int p){
             if(x!=p)return true;
         }
     }
+    vis[c] = false;
     return false;
 }
 bool checkCycle(int n){
+    vector<bool> vis(n,false);
+    bool flag = false;
     for(int i=0;i<n;i++){
-        if(dfs(i,-1))return true;
+        vis[i] = true;
+        if(dfs(i,-1,vis))return true;
+        vis[i] = false;
     }
     return false;
 }
 
 int main(){
     int t;
-    cin>>t;
+    // cin>>t;
+    t = 1;
     while(t--){
         int n,m,i;
         cin>>n>>m;
